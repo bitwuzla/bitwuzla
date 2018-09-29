7f6d70cd8b4ce4c2d960c60c245c3a0493815e08
Aina Niemetz <aina.niemetz@gmail.com>
2018-09-29 11:43:07 -0700

    FP smt2 parser: Parse chainable fp funs.

--- src/parser/bzlasmt2.c
+++ src/parser/bzlasmt2.c
@@ -2369,6 +2369,43 @@ close_term_bin_fp_fun(BzlaSMT2Parser *parser,
  * item_cur[1] is the first argument, ..., item_cur[nargs] is the last argument.
  */
 static int32_t
+close_term_bin_fp_fun_chainable(BzlaSMT2Parser *parser,
+                                BzlaSMT2Item *item_open,
+                                BzlaSMT2Item *item_cur,
+                                uint32_t nargs)
+//       BoolectorNode *(*fun) (Bzla *,
+//                              BoolectorNode *,
+//                              BoolectorNode *) )
+{
+  assert(parser);
+  assert(item_open);
+  assert(item_cur);
+  // assert (fun);
+
+  assert(item_cur->tag == BZLA_FP_EQ_TAG_SMT2
+         || item_cur->tag == BZLA_FP_LEQ_TAG_SMT2
+         || item_cur->tag == BZLA_FP_LT_TAG_SMT2
+         || item_cur->tag == BZLA_FP_GEQ_TAG_SMT2
+         || item_cur->tag == BZLA_FP_GT_TAG_SMT2);
+
+  BoolectorNode *exp;
+
+  if (!check_nargs_smt2(parser, item_cur, nargs, 2)) return 0;
+  if (!check_arg_sorts_match_smt2(parser, item_cur, 0, 2)) return 0;
+  // TODO: check all args FP
+  // FP STUB
+  exp = boolector_true(parser->bzla);
+  // exp = fun (parser->bzla, item_cur[1].exp, item_cur[2].exp);
+  ////
+  release_exp_and_overwrite(parser, item_open, item_cur, 0, nargs, exp);
+  return 1;
+}
+/**
+ * item_open and item_cur point to items on the parser work stack.
+ * If if nargs > 0, we expect nargs SMT2Items on the stack after item_cur:
+ * item_cur[1] is the first argument, ..., item_cur[nargs] is the last argument.
+ */
+static int32_t
 close_term_bin_rm_fp_fun(BzlaSMT2Parser *parser,
                          BzlaSMT2Item *item_open,
                          BzlaSMT2Item *item_cur,
@@ -3386,6 +3423,46 @@ close_term(BzlaSMT2Parser *parser)
   else if (tag == BZLA_FP_MAX_TAG_SMT2)
   {
     if (!close_term_bin_fp_fun(parser, item_open, item_cur, nargs))
+    {
+      return 0;
+    }
+  }
+  /* FP: fp.eq -------------------------------------------------------------- */
+  else if (tag == BZLA_FP_LEQ_TAG_SMT2)
+  {
+    if (!close_term_bin_fp_fun_chainable(parser, item_open, item_cur, nargs))
+    {
+      return 0;
+    }
+  }
+  /* FP: fp.leq ------------------------------------------------------------- */
+  else if (tag == BZLA_FP_LEQ_TAG_SMT2)
+  {
+    if (!close_term_bin_fp_fun_chainable(parser, item_open, item_cur, nargs))
+    {
+      return 0;
+    }
+  }
+  /* FP: fp.lt -------------------------------------------------------------- */
+  else if (tag == BZLA_FP_LT_TAG_SMT2)
+  {
+    if (!close_term_bin_fp_fun_chainable(parser, item_open, item_cur, nargs))
+    {
+      return 0;
+    }
+  }
+  /* FP: fp.geq ------------------------------------------------------------- */
+  else if (tag == BZLA_FP_GEQ_TAG_SMT2)
+  {
+    if (!close_term_bin_fp_fun_chainable(parser, item_open, item_cur, nargs))
+    {
+      return 0;
+    }
+  }
+  /* FP: fp.gt -------------------------------------------------------------- */
+  else if (tag == BZLA_FP_GT_TAG_SMT2)
+  {
+    if (!close_term_bin_fp_fun_chainable(parser, item_open, item_cur, nargs))
     {
       return 0;
     }
