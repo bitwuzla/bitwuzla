d66f71b0ca4a4edb486b73987925e128f8f0ef0e
Aina Niemetz <aina.niemetz@gmail.com>
2019-12-16 13:06:27 -0800

    invutils: Add documentation.

--- src/bzlainvutils.h
+++ src/bzlainvutils.h
@@ -12,63 +12,75 @@
 
 #include "bzlabv.h"
 
-/*------------------------------------------------------------------------*/
+/* -------------------------------------------------------------------------- */
 
+/** Check invertibility of x + s = t or s + x = t when solved for x. */
 bool bzla_is_inv_add(BzlaMemMgr *mm,
                      const BzlaBitVector *t,
                      const BzlaBitVector *s,
                      uint32_t pos_x);
 
+/** Check invertibility of x & s = t or s & x = t when solved for x. */
 bool bzla_is_inv_and(BzlaMemMgr *mm,
                      const BzlaBitVector *t,
                      const BzlaBitVector *s,
                      uint32_t pos_x);
 
+/** Check invertibility of x o s = t or s o x = t when solved for x. */
 bool bzla_is_inv_concat(BzlaMemMgr *mm,
                         const BzlaBitVector *t,
                         const BzlaBitVector *s,
                         uint32_t pos_x);
 
+/** Check invertibility of x & s = t or s & x = t when solved for x. */
 bool bzla_is_inv_eq(BzlaMemMgr *mm,
                     const BzlaBitVector *t,
                     const BzlaBitVector *s,
                     uint32_t pos_x);
 
+/** Check invertibility of x * s = t or s * x = t when solved for x. */
 bool bzla_is_inv_mul(BzlaMemMgr *mm,
                      const BzlaBitVector *t,
                      const BzlaBitVector *s,
                      uint32_t pos_x);
 
+/** Check invertibility of x | s = t or s | x = t when solved for x. */
 bool bzla_is_inv_or(BzlaMemMgr *mm,
                     const BzlaBitVector *t,
                     const BzlaBitVector *s,
                     uint32_t pos_x);
 
+/** Check invertibility of x << s = t or s << x = t when solved for x. */
 bool bzla_is_inv_sll(BzlaMemMgr *mm,
                      const BzlaBitVector *t,
                      const BzlaBitVector *s,
                      uint32_t pos_x);
 
+/** Check invertibility of x >>a s = t or s >>a x = t when solved for x. */
 bool bzla_is_inv_sra(BzlaMemMgr *mm,
                      const BzlaBitVector *t,
                      const BzlaBitVector *s,
                      uint32_t pos_x);
 
+/** Check invertibility of x >> s = t or s >> x = t when solved for x. */
 bool bzla_is_inv_srl(BzlaMemMgr *mm,
                      const BzlaBitVector *t,
                      const BzlaBitVector *s,
                      uint32_t pos_x);
 
+/** Check invertibility of x / s = t or s / x = t when solved for x. */
 bool bzla_is_inv_udiv(BzlaMemMgr *mm,
                       const BzlaBitVector *t,
                       const BzlaBitVector *s,
                       uint32_t pos_x);
 
+/** Check invertibility of x < s = t or s < x = t when solved for x. */
 bool bzla_is_inv_ult(BzlaMemMgr *mm,
                      const BzlaBitVector *t,
                      const BzlaBitVector *s,
                      uint32_t pos_x);
 
+/** Check invertibility of x % s = t or s % x = t when solved for x. */
 bool bzla_is_inv_urem(BzlaMemMgr *mm,
                       const BzlaBitVector *t,
                       const BzlaBitVector *s,
