07931ef7c75f21fd2314c31aac1d7e514a9afbe5
Aina Niemetz <aina.niemetz@gmail.com>
2020-04-27 11:02:27 -0700

    prop: More cleanup in cons_srl_const and cons_sll_const.

--- src/bzlaproputils.c
+++ src/bzlaproputils.c
@@ -2017,7 +2017,7 @@ bzla_proputils_cons_sll_const(Bzla *bzla, BzlaPropInfo *pi)
 #endif
   int32_t pos_x;
   uint32_t i, r, bw, bw_r, ctz_t;
-  BzlaBitVector *res, *left, *right, *zero, *max, *tmp, *t_slice;
+  BzlaBitVector *res, *left, *right, *max, *tmp, *t_slice;
   BzlaBvDomain *x_slice;
   const BzlaBvDomain *x;
   const BzlaBitVector *t;
@@ -2049,19 +2049,16 @@ bzla_proputils_cons_sll_const(Bzla *bzla, BzlaPropInfo *pi)
     else
     {
       assert(ctz_t < bw);
-      max  = bzla_bv_uint64_to_bv(mm, ctz_t, bw);
-      zero = bzla_bv_zero(mm, bw);
-      bzla_bvdomain_gen_init_range(mm, &bzla->rng, &gen, x, zero, max);
+      max = bzla_bv_uint64_to_bv(mm, ctz_t, bw);
+      bzla_bvdomain_gen_init_range(mm, &bzla->rng, &gen, x, 0, max);
       if (!bzla_bvdomain_gen_has_next(&gen))
       {
         /* non-recoverable conflict */
-        bzla_bv_free(mm, zero);
         bzla_bv_free(mm, max);
         bzla_bvdomain_gen_delete(&gen);
         return NULL;
       }
       res = bzla_bv_copy(mm, bzla_bvdomain_gen_random(&gen));
-      bzla_bv_free(mm, zero);
       bzla_bv_free(mm, max);
       bzla_bvdomain_gen_delete(&gen);
     }
@@ -2124,7 +2121,7 @@ bzla_proputils_cons_srl_const(Bzla *bzla, BzlaPropInfo *pi)
 #endif
   int32_t pos_x;
   uint32_t i, r, bw, bw_l, clz_t;
-  BzlaBitVector *res, *left, *right, *zero, *max, *tmp, *t_slice;
+  BzlaBitVector *res, *left, *right, *max, *tmp, *t_slice;
   BzlaBvDomain *x_slice;
   const BzlaBvDomain *x;
   BzlaBvDomainGenerator gen;
@@ -2149,19 +2146,16 @@ bzla_proputils_cons_srl_const(Bzla *bzla, BzlaPropInfo *pi)
   else if (pos_x)
   {
     assert(clz_t < bw);
-    max  = bzla_bv_uint64_to_bv(mm, clz_t, bw);
-    zero = bzla_bv_zero(mm, bw);
-    bzla_bvdomain_gen_init_range(mm, &bzla->rng, &gen, x, zero, max);
+    max = bzla_bv_uint64_to_bv(mm, clz_t, bw);
+    bzla_bvdomain_gen_init_range(mm, &bzla->rng, &gen, x, 0, max);
     if (!bzla_bvdomain_gen_has_next(&gen))
     {
       /* non-recoverable conflict */
-      bzla_bv_free(mm, zero);
       bzla_bv_free(mm, max);
       bzla_bvdomain_gen_delete(&gen);
       return NULL;
     }
     res = bzla_bv_copy(mm, bzla_bvdomain_gen_random(&gen));
-    bzla_bv_free(mm, zero);
     bzla_bv_free(mm, max);
     bzla_bvdomain_gen_delete(&gen);
   }
