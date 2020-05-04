c4d457fe8e8a7ea0c51a447e6af6d3025ce488a8
Mathias Preiner <mathias.preiner@gmail.com>
2020-05-04 13:25:04 -0700

    Remove incorrect rewrite.

--- src/bzlarewrite.c
+++ src/bzlarewrite.c
@@ -3196,32 +3196,6 @@ apply_bool_slt(Bzla *bzla, BzlaNode *e0, BzlaNode *e1)
 }
 
 /*
- * match:  (a::b) < (a::c)
- * result: b < c
- */
-static inline bool
-applies_concat_upper_slt(Bzla *bzla, BzlaNode *e0, BzlaNode *e1)
-{
-  return bzla_opt_get(bzla, BZLA_OPT_REWRITE_LEVEL) > 2
-         && bzla->rec_rw_calls < BZLA_REC_RW_BOUND && !bzla_node_is_inverted(e0)
-         && !bzla_node_is_inverted(e1) && bzla_node_is_bv_concat(e0)
-         && e0->kind == e1->kind && e0->e[0] == e1->e[0];
-}
-
-static inline BzlaNode *
-apply_concat_upper_slt(Bzla *bzla, BzlaNode *e0, BzlaNode *e1)
-{
-  assert(applies_concat_upper_slt(bzla, e0, e1));
-
-  BzlaNode *result;
-
-  BZLA_INC_REC_RW_CALL(bzla);
-  result = rewrite_bv_slt_exp(bzla, e0->e[1], e1->e[1]);
-  BZLA_DEC_REC_RW_CALL(bzla);
-  return result;
-}
-
-/*
  * match:  (b::a) < (c::a)
  * result: b < c
  */
@@ -7551,7 +7525,6 @@ rewrite_bv_slt_exp(Bzla *bzla, BzlaNode *e0, BzlaNode *e1)
     ADD_RW_RULE(special_const_rhs_binary_exp, BZLA_BV_SLT_NODE, e0, e1);
     ADD_RW_RULE(false_lt, e0, e1);
     ADD_RW_RULE(bool_slt, e0, e1);
-    ADD_RW_RULE(concat_upper_slt, e0, e1);
     ADD_RW_RULE(concat_lower_slt, e0, e1);
     ADD_RW_RULE(bcond_slt, e0, e1);
 
