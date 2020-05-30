51296f9fe2b482b3d022e4a74e5b3ae8225e91d4
Aina Niemetz <aina.niemetz@gmail.com>
2020-05-29 19:58:00 -0700

    prop: Document consutils header.

--- src/bzlaconsutils.h
+++ src/bzlaconsutils.h
@@ -13,18 +13,82 @@
 
 typedef struct BzlaPropInfo BzlaPropInfo;
 
+/**
+ * Check consistency of x + s = t or s + x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_add_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of x & s = t or s & x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_and_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of x ^ s = t or s ^ x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_xor_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of (x = s) = t or (s = x) = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_eq_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of (x < s) = t or (s < x) = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_ult_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of (x <s s) = t or (s <s x) = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_slt_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of x << s = t or s << x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_sll_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of x >> s = t or s >> x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_srl_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of x >>a s = t or s >>a x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_sra_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of x * s = t or s * x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_mul_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of x / s = t or s / x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_udiv_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of x % s = t or s % x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_urem_const(Bzla *bzla, BzlaPropInfo *pi);
+
+/**
+ * Check consistency of x o s = t or s o x = t when solved for x with
+ * respect to const bits in x.
+ */
 bool bzla_is_cons_concat_const(Bzla *bzla, BzlaPropInfo *pi);
 
 #endif
