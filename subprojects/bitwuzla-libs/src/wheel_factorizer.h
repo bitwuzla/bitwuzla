430da3535dae8fa2f6403d7fbdb7788351396bcf
Aina Niemetz <aina.niemetz@gmail.com>
2021-01-11 10:58:26 -0800

    wheel factorizer: Document header.

--- subprojects/bitwuzla-libs/src/wheel_factorizer.h
+++ subprojects/bitwuzla-libs/src/wheel_factorizer.h
@@ -11,17 +11,36 @@ namespace bzlals {
 class WheelFactorizer
 {
  public:
+  /**
+   * Constructor.
+   * n    : The bit-vector value to factorize.
+   * limit: The limit for max number of iterations.
+   */
   WheelFactorizer(const BitVector& n, uint64_t limit);
-  BitVector* next();
+  /**
+   * Get next factor.
+   * Returns nullptr when no next factor exists.
+   */
+  const BitVector* next();
 
  private:
+  /**
+   * The value to factorize.
+   * Updated at each call to next() to d_num / d_fact if d_fact is a factor
+   * of d_num, i.e., if d_num % d_fact = 0. */
   BitVector d_num;
+  /** The current factor. */
   BitVector d_fact;
+  /** Bit-vector value one. */
   BitVector d_one;
+  /** Bit-vector value two. */
   BitVector d_two;
+  /** Bit-vector value four. */
   BitVector d_four;
+  /** Bit-vector value six. */
   BitVector d_six;
 
+  /** The increments applied to d_fact for a {2, 3, 5} wheel. */
   BitVector* d_inc[11];
 
   bool d_done  = false;
