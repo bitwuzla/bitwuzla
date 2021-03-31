7b4a9095428d01c1f708248795224f8bf6b0e1b0
Aina Niemetz <aina.niemetz@gmail.com>
2021-03-31 16:29:55 -0700

    Use patched version of symfpu.

--- contrib/setup-symfpu.sh
+++ contrib/setup-symfpu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source "$(dirname "$0")/setup-utils.sh"
+CONTRIB_DIR=$(dirname $(realpath -s $0))
+
+source "$CONTRIB_DIR/setup-utils.sh"
 
 SYMFPU_DIR="${DEPS_DIR}/symfpu"
 
@@ -10,5 +12,6 @@ commit="8fbe139bf0071cbe0758d2f6690a546c69ff0053"
 git clone https://github.com/martin-cs/symfpu.git "${SYMFPU_DIR}"
 cd "${SYMFPU_DIR}"
 git checkout $commit
+git apply "$CONTRIB_DIR/symfpu_202012001.patch"
 install_include core symfpu
 install_include utils symfpu
