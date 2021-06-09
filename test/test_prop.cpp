8c1c40e1269937bb5625d0c4d811721ab8c63013
Aina Niemetz <aina.niemetz@gmail.com>
2021-06-09 16:11:12 -0700

    prop: Refactor tests to merge test helpers into one.

--- test/test_prop.cpp
+++ test/test_prop.cpp
@@ -42,8 +42,15 @@ class TestProp : public TestPropCommon
                                    const bzlals::BitVector& s0_val,
                                    const bzlals::BitVector& s1_val);
 
-  void test_binary(BzlaNodeKind kind);
-  void test_ite();
+  void test_prop_aux(BzlaNodeKind kind,
+                     const bzlals::BitVectorDomain* s0,
+                     const bzlals::BitVectorDomain* s1,
+                     const bzlals::BitVectorDomain* s2,
+                     const bzlals::BitVector* s0_val,
+                     const bzlals::BitVector* s1_val,
+                     const bzlals::BitVector* s2_val);
+
+  void test_prop(BzlaNodeKind kind);
   // void test_not();
   // void test_extract();
   // void test_sext();
@@ -120,90 +127,133 @@ TestProp::eval_op_binary(BzlaNodeKind kind,
 }
 
 void
-TestProp::test_binary(BzlaNodeKind kind)
+TestProp::test_prop_aux(BzlaNodeKind kind,
+                        const bzlals::BitVectorDomain* s0,
+                        const bzlals::BitVectorDomain* s1,
+                        const bzlals::BitVectorDomain* s2,
+                        const bzlals::BitVector* s0_val,
+                        const bzlals::BitVector* s1_val,
+                        const bzlals::BitVector* s2_val)
 {
-  std::vector<std::string> xvalues;
-  gen_xvalues(d_test_bw, xvalues);
+  assert(s0_val);
 
-  for (const std::string& s0_domain_value : xvalues)
+  Bzla* bzla      = create_bzla();
+  BzlaSortId sort = bzla_sort_bv(bzla, d_test_bw);
+  BzlaSortId sort0 =
+      s2_val ? bzla_sort_bv(bzla, 1) : bzla_sort_copy(bzla, sort);
+  BzlaNode* var0 = bzla_exp_var(bzla, sort0, 0);
+  BzlaNode* var1 = bzla_exp_var(bzla, sort, 0);
+  BzlaNode *var2, *op;
+  bzlals::BitVector t_val;
+
+  if (s2_val)
   {
-    bzlals::BitVectorDomain s0(s0_domain_value);
-    bzlals::BitVectorDomainGenerator gens0(s0);
-    do
-    {
-      bzlals::BitVector s0_val = gens0.has_next() ? gens0.next() : s0.lo();
-      for (const std::string& s1_domain_value : xvalues)
-      {
-        bzlals::BitVectorDomain s1(s1_domain_value);
-        bzlals::BitVectorDomainGenerator gens1(s1);
-        do
-        {
-          bzlals::BitVector s1_val = gens1.has_next() ? gens1.next() : s1.lo();
-          bzlals::BitVector t_val  = eval_op_binary(kind, s0_val, s1_val);
-
-          Bzla* bzla      = create_bzla();
-          BzlaSortId sort = bzla_sort_bv(bzla, d_test_bw);
-          BzlaNode* var0  = bzla_exp_var(bzla, sort, 0);
-          BzlaNode* var1  = bzla_exp_var(bzla, sort, 0);
-          BzlaNode* op    = mk_op_binary(bzla, kind, var0, var1);
-
-          BzlaBitVector* t_bv =
-              bzla_bv_const(bzla->mm, t_val.to_string().c_str(), t_val.size());
-          BzlaNode* t  = bzla_exp_bv_const(bzla, t_bv);
-          BzlaNode* eq = bzla_exp_eq(bzla, op, t);
-          bzla_assert_exp(bzla, eq);
-          bzla_synthesize_exp(bzla, var0, nullptr);
-          bzla_synthesize_exp(bzla, var1, nullptr);
-          assert(!bzla_node_is_inverted(var0));
-          assert(!bzla_node_is_inverted(var1));
-          for (uint32_t i = 0, n = s0.size(); i < n; ++i)
-          {
-            if (s0.is_fixed_bit(i))
-            {
-              uint32_t ai    = d_test_bw - 1 - i;
-              BzlaAIGVec* av = var0->av;
-              assert(!bzla_aig_is_const(av->aigs[ai]));
-              bzla_aig_release(bzla->avmgr->amgr, av->aigs[ai]);
-              av->aigs[ai] =
-                  s0.is_fixed_bit_true(i) ? BZLA_AIG_TRUE : BZLA_AIG_FALSE;
-            }
-          }
-          for (uint32_t i = 0, n = s1.size(); i < n; ++i)
-          {
-            if (s1.is_fixed_bit(i))
-            {
-              uint32_t ai    = d_test_bw - 1 - i;
-              BzlaAIGVec* av = var1->av;
-              assert(!bzla_aig_is_const(av->aigs[ai]));
-              bzla_aig_release(bzla->avmgr->amgr, av->aigs[ai]);
-              av->aigs[ai] =
-                  s1.is_fixed_bit_true(i) ? BZLA_AIG_TRUE : BZLA_AIG_FALSE;
-            }
-          }
-
-          int32_t res = bzla_check_sat(bzla, -1, -1);
-          assert(res == BZLA_RESULT_SAT || res == BZLA_RESULT_UNSAT);
-
-          bzla_bv_free(bzla->mm, t_bv);
-          bzla_node_release(bzla, t);
-          bzla_node_release(bzla, eq);
-          bzla_node_release(bzla, op);
-          bzla_node_release(bzla, var0);
-          bzla_node_release(bzla, var1);
-          bzla_sort_release(bzla, sort);
-          bzla_delete(bzla);
-        } while (gens1.has_next());
-      }
-    } while (gens0.has_next());
+    /* ternary */
+    assert(s1_val && s0_val);
+    var2  = bzla_exp_var(bzla, sort, 0);
+    op    = bzla_exp_cond(bzla, var0, var1, var2);
+    t_val = bzlals::BitVector::bvite(*s0_val, *s1_val, *s2_val);
   }
+  else if (s1_val)
+  {
+    /* binary */
+    assert(s0_val);
+    op    = mk_op_binary(bzla, kind, var0, var1);
+    t_val = eval_op_binary(kind, *s0_val, *s1_val);
+  }
+  else
+  {
+    /* unary */
+  }
+
+  BzlaBitVector* t_bv =
+      bzla_bv_const(bzla->mm, t_val.to_string().c_str(), t_val.size());
+  BzlaNode* t  = bzla_exp_bv_const(bzla, t_bv);
+  BzlaNode* eq = bzla_exp_eq(bzla, op, t);
+
+  bzla_assert_exp(bzla, eq);
+
+  bzla_synthesize_exp(bzla, var1, nullptr);
+  assert(!bzla_node_is_inverted(var0));
+  assert(!bzla_node_is_inverted(var1));
+
+  if (s0_val)
+  {
+    bzla_synthesize_exp(bzla, var0, nullptr);
+    assert(!bzla_node_is_inverted(var0));
+    for (uint32_t i = 0, n = s0->size(); i < n; ++i)
+    {
+      if (s0->is_fixed_bit(i))
+      {
+        uint32_t ai    = n - 1 - i;
+        BzlaAIGVec* av = var0->av;
+        assert(!bzla_aig_is_const(av->aigs[ai]));
+        bzla_aig_release(bzla->avmgr->amgr, av->aigs[ai]);
+        av->aigs[ai] =
+            s0->is_fixed_bit_true(i) ? BZLA_AIG_TRUE : BZLA_AIG_FALSE;
+      }
+    }
+  }
+
+  if (s1_val)
+  {
+    bzla_synthesize_exp(bzla, var1, nullptr);
+    assert(!bzla_node_is_inverted(var1));
+    for (uint32_t i = 0, n = s1->size(); i < n; ++i)
+    {
+      if (s1->is_fixed_bit(i))
+      {
+        uint32_t ai    = n - 1 - i;
+        BzlaAIGVec* av = var1->av;
+        assert(!bzla_aig_is_const(av->aigs[ai]));
+        bzla_aig_release(bzla->avmgr->amgr, av->aigs[ai]);
+        av->aigs[ai] =
+            s1->is_fixed_bit_true(i) ? BZLA_AIG_TRUE : BZLA_AIG_FALSE;
+      }
+    }
+  }
+
+  if (s2_val)
+  {
+    bzla_synthesize_exp(bzla, var2, nullptr);
+    assert(!bzla_node_is_inverted(var2));
+    for (uint32_t i = 0, n = s2->size(); i < n; ++i)
+    {
+      if (s2->is_fixed_bit(i))
+      {
+        uint32_t ai    = n - 1 - i;
+        BzlaAIGVec* av = var2->av;
+        assert(!bzla_aig_is_const(av->aigs[ai]));
+        bzla_aig_release(bzla->avmgr->amgr, av->aigs[ai]);
+        av->aigs[ai] =
+            s2->is_fixed_bit_true(i) ? BZLA_AIG_TRUE : BZLA_AIG_FALSE;
+      }
+    }
+  }
+
+  int32_t res = bzla_check_sat(bzla, -1, -1);
+  assert(res == BZLA_RESULT_SAT || res == BZLA_RESULT_UNSAT);
+
+  bzla_bv_free(bzla->mm, t_bv);
+  bzla_node_release(bzla, t);
+  bzla_node_release(bzla, eq);
+  bzla_node_release(bzla, op);
+  bzla_node_release(bzla, var0);
+  if (s1_val) bzla_node_release(bzla, var1);
+  if (s2_val) bzla_node_release(bzla, var2);
+  bzla_sort_release(bzla, sort0);
+  bzla_sort_release(bzla, sort);
+  bzla_delete(bzla);
 }
 
 void
-TestProp::test_ite()
+TestProp::test_prop(BzlaNodeKind kind)
 {
-  std::vector<std::string> s0values = {"x", "0", "1"};
+  std::vector<std::string> xvalues0 = {"x", "0", "1"};
   std::vector<std::string> xvalues;
   gen_xvalues(d_test_bw, xvalues);
+  std::vector<std::string>& s0values =
+      kind == BZLA_COND_NODE ? xvalues0 : xvalues;
 
   for (const std::string& s0_domain_value : s0values)
   {
@@ -219,83 +269,24 @@ TestProp::test_ite()
         do
         {
           bzlals::BitVector s1_val = gens1.has_next() ? gens1.next() : s1.lo();
-
-          for (const std::string& s2_domain_value : xvalues)
+          if (kind == BZLA_COND_NODE)
           {
-            bzlals::BitVectorDomain s2(s2_domain_value);
-            bzlals::BitVectorDomainGenerator gens2(s2);
-            do
+            for (const std::string& s2_domain_value : xvalues)
             {
-              bzlals::BitVector s2_val =
-                  gens2.has_next() ? gens2.next() : s2.lo();
-              bzlals::BitVector t_val =
-                  bzlals::BitVector::bvite(s0_val, s1_val, s2_val);
-
-              Bzla* bzla       = create_bzla();
-              BzlaSortId sort1 = bzla_sort_bv(bzla, 1);
-              BzlaSortId sort  = bzla_sort_bv(bzla, d_test_bw);
-              BzlaNode* var0   = bzla_exp_var(bzla, sort1, 0);
-              BzlaNode* var1   = bzla_exp_var(bzla, sort, 0);
-              BzlaNode* var2   = bzla_exp_var(bzla, sort, 0);
-              BzlaNode* op     = bzla_exp_cond(bzla, var0, var1, var2);
-
-              BzlaBitVector* t_bv = bzla_bv_const(
-                  bzla->mm, t_val.to_string().c_str(), t_val.size());
-              BzlaNode* t  = bzla_exp_bv_const(bzla, t_bv);
-              BzlaNode* eq = bzla_exp_eq(bzla, op, t);
-              bzla_assert_exp(bzla, eq);
-              bzla_synthesize_exp(bzla, var0, nullptr);
-              bzla_synthesize_exp(bzla, var1, nullptr);
-              bzla_synthesize_exp(bzla, var2, nullptr);
-              assert(!bzla_node_is_inverted(var0));
-              assert(!bzla_node_is_inverted(var1));
-              assert(!bzla_node_is_inverted(var2));
+              bzlals::BitVectorDomain s2(s2_domain_value);
+              bzlals::BitVectorDomainGenerator gens2(s2);
+              do
               {
-                BzlaAIGVec* av = var0->av;
-                assert(!bzla_aig_is_const(av->aigs[0]));
-                bzla_aig_release(bzla->avmgr->amgr, av->aigs[0]);
-                av->aigs[0] =
-                    s0.is_fixed_bit_true(0) ? BZLA_AIG_TRUE : BZLA_AIG_FALSE;
-              }
-              for (uint32_t i = 0, n = s1.size(); i < n; ++i)
-              {
-                if (s1.is_fixed_bit(i))
-                {
-                  uint32_t ai    = d_test_bw - 1 - i;
-                  BzlaAIGVec* av = var1->av;
-                  assert(!bzla_aig_is_const(av->aigs[ai]));
-                  bzla_aig_release(bzla->avmgr->amgr, av->aigs[ai]);
-                  av->aigs[ai] =
-                      s1.is_fixed_bit_true(i) ? BZLA_AIG_TRUE : BZLA_AIG_FALSE;
-                }
-              }
-              for (uint32_t i = 0, n = s2.size(); i < n; ++i)
-              {
-                if (s2.is_fixed_bit(i))
-                {
-                  uint32_t ai    = d_test_bw - 1 - i;
-                  BzlaAIGVec* av = var2->av;
-                  assert(!bzla_aig_is_const(av->aigs[ai]));
-                  bzla_aig_release(bzla->avmgr->amgr, av->aigs[ai]);
-                  av->aigs[ai] =
-                      s2.is_fixed_bit_true(i) ? BZLA_AIG_TRUE : BZLA_AIG_FALSE;
-                }
-              }
-
-              int32_t res = bzla_check_sat(bzla, -1, -1);
-              assert(res == BZLA_RESULT_SAT || res == BZLA_RESULT_UNSAT);
-
-              bzla_bv_free(bzla->mm, t_bv);
-              bzla_node_release(bzla, t);
-              bzla_node_release(bzla, eq);
-              bzla_node_release(bzla, op);
-              bzla_node_release(bzla, var0);
-              bzla_node_release(bzla, var1);
-              bzla_node_release(bzla, var2);
-              bzla_sort_release(bzla, sort);
-              bzla_sort_release(bzla, sort1);
-              bzla_delete(bzla);
-            } while (gens2.has_next());
+                bzlals::BitVector s2_val =
+                    gens2.has_next() ? gens2.next() : s2.lo();
+                test_prop_aux(
+                    BZLA_COND_NODE, &s0, &s1, &s2, &s0_val, &s1_val, &s2_val);
+              } while (gens2.has_next());
+            }
+          }
+          else
+          {
+            test_prop_aux(kind, &s0, &s1, nullptr, &s0_val, &s1_val, nullptr);
           }
         } while (gens1.has_next());
       }
@@ -303,29 +294,29 @@ TestProp::test_ite()
   }
 }
 
-TEST_F(TestProp, add) { test_binary(BZLA_BV_ADD_NODE); }
+TEST_F(TestProp, add) { test_prop(BZLA_BV_ADD_NODE); }
 
-TEST_F(TestProp, and) { test_binary(BZLA_BV_AND_NODE); }
+TEST_F(TestProp, and) { test_prop(BZLA_BV_AND_NODE); }
 
-TEST_F(TestProp, concat) { test_binary(BZLA_BV_CONCAT_NODE); }
+TEST_F(TestProp, concat) { test_prop(BZLA_BV_CONCAT_NODE); }
 
-TEST_F(TestProp, eq) { test_binary(BZLA_BV_EQ_NODE); }
+TEST_F(TestProp, eq) { test_prop(BZLA_BV_EQ_NODE); }
 
-TEST_F(TestProp, mul) { test_binary(BZLA_BV_MUL_NODE); }
+TEST_F(TestProp, mul) { test_prop(BZLA_BV_MUL_NODE); }
 
-TEST_F(TestProp, shl) { test_binary(BZLA_BV_SLL_NODE); }
+TEST_F(TestProp, shl) { test_prop(BZLA_BV_SLL_NODE); }
 
-TEST_F(TestProp, shr) { test_binary(BZLA_BV_SRL_NODE); }
+TEST_F(TestProp, shr) { test_prop(BZLA_BV_SRL_NODE); }
 
-TEST_F(TestProp, udiv) { test_binary(BZLA_BV_UDIV_NODE); }
+TEST_F(TestProp, udiv) { test_prop(BZLA_BV_UDIV_NODE); }
 
-TEST_F(TestProp, ult) { test_binary(BZLA_BV_ULT_NODE); }
+TEST_F(TestProp, ult) { test_prop(BZLA_BV_ULT_NODE); }
 
-TEST_F(TestProp, slt) { test_binary(BZLA_BV_SLT_NODE); }
+TEST_F(TestProp, slt) { test_prop(BZLA_BV_SLT_NODE); }
 
-TEST_F(TestProp, urem) { test_binary(BZLA_BV_UREM_NODE); }
+TEST_F(TestProp, urem) { test_prop(BZLA_BV_UREM_NODE); }
 
-TEST_F(TestProp, ite) { test_ite(); }
+TEST_F(TestProp, ite) { test_prop(BZLA_COND_NODE); }
 
 // TEST_F(TestProp, not ) { test_not(); }
 
