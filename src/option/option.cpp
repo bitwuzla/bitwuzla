aa021496f4dc196735431b240a93c2b69944b31c
Aina Niemetz <aina.niemetz@gmail.com>
2022-09-15 11:17:55 -0700

    opt: More, document, move code to .cpp file.

--- src/option/option.cpp
+++ src/option/option.cpp
@@ -1,1 +1,100 @@
 #include "option/option.h"
+
+#include <cassert>
+
+namespace bzla::options {
+
+/* --- OptionInfo::Enum public ---------------------------------------------- */
+
+OptionInfo::Enum::Enum(uint64_t val, const Enum2StringMap& enum2string)
+    : d_value(val), d_default(val), d_enum2string(enum2string)
+{
+  for (const auto& p : enum2string)
+  {
+    d_string2enum.emplace(p.second, p.first);
+  }
+}
+
+/* --- Options public ------------------------------------------------------- */
+
+Options::Options() : d_options(static_cast<size_t>(Option::NUM_OPTIONS))
+{
+  init(Option::INCREMENTAL,
+       incremental,
+       false,
+       "incremental usage",
+       "incremental",
+       "i");
+  init(Option::LOG_LEVEL, log_level, 0, 0, 3, "log level", "log-level", "l");
+  init(Option::SAT_SOLVER,
+       sat_solver,
+       SatSolver::CADICAL,
+       {{SatSolver::CADICAL, "cadical"},
+        {SatSolver::CRYPTOMINISAT, "cms"},
+        {SatSolver::KISSAT, "kissat"},
+        {SatSolver::LINGELING, "lingeling"}},
+       "sat solver",
+       "sat-engine",
+       "SE");
+  assert(d_num_initialized == static_cast<size_t>(Option::NUM_OPTIONS));
+}
+
+void
+Options::init(Option name,
+              bool& option,
+              bool value,
+              const char* desc,
+              const char* lng,
+              const char* shrt,
+              bool is_expert)
+{
+  assert(desc);
+  assert(lng);
+
+  d_num_initialized++;
+  option                               = value;
+  d_options[static_cast<size_t>(name)] = {value, desc, lng, shrt, is_expert};
+}
+
+template <typename T>
+void
+Options::init(Option name,
+              T& option,
+              T value,
+              const OptionInfo::Enum2StringMap& enum2string,
+              const char* desc,
+              const char* lng,
+              const char* shrt,
+              bool is_expert)
+{
+  assert(desc);
+  assert(lng);
+  d_num_initialized++;
+  option                               = value;
+  d_options[static_cast<size_t>(name)] = {
+      static_cast<uint64_t>(value), enum2string, desc, lng, shrt, is_expert};
+}
+
+void
+Options::init(Option name,
+              uint64_t& option,
+              uint64_t value,
+              uint64_t min,
+              uint64_t max,
+              const char* desc,
+              const char* lng,
+              const char* shrt,
+              bool is_expert)
+{
+  assert(min <= value);
+  assert(value <= max);
+  assert(desc);
+  assert(lng);
+
+  d_num_initialized++;
+  option                               = value;
+  d_options[static_cast<size_t>(name)] = {
+      value, min, max, desc, lng, shrt, is_expert};
+}
+
+}  // namespace bzla::options
--- src/option/option.h
+++ src/option/option.h
@@ -10,14 +10,18 @@
 
 namespace bzla::options {
 
+/* -------------------------------------------------------------------------- */
+
 enum class Option
 {
-  SAT_ENGINE,   // enum
+  SAT_SOLVER,   // enum
   INCREMENTAL,  // bool
-  LOGLEVEL,     // numeric
+  LOG_LEVEL,    // numeric
 
   NUM_OPTIONS
 };
+
+/* -------------------------------------------------------------------------- */
 
 enum SatSolver
 {
@@ -27,152 +31,232 @@ enum SatSolver
   LINGELING,
 };
 
+/* -------------------------------------------------------------------------- */
+
 class OptionInfo
 {
  public:
+  using String2EnumMap = std::unordered_map<const char*, uint64_t>;
+  using Enum2StringMap = std::unordered_map<uint64_t, const char*>;
+
+  /** The value data for Boolean options. */
   struct Boolean
   {
-    Boolean(bool val, bool dflt) : d_value(val), d_default(dflt) {}
+    /**
+     * Constructor.
+     *
+     * On construction, given Boolean value determines the initial and the
+     * default value of the option.
+     *
+     * @param val The boolean initial and default value of the option.
+     */
+    Boolean(bool val) : d_value(val), d_default(val) {}
+    /** The current value of a Boolean option. */
     bool d_value;
+    /** The default value of a Boolean option. */
     bool d_default;
   };
 
+  /** The value data of options with enum values. */
   struct Enum
   {
-    Enum(uint64_t val,
-         uint64_t dflt,
-         const std::unordered_map<const char*, uint64_t> string2enum,
-         const std::unordered_map<uint64_t, const char*> enum2string)
-        : d_value(val),
-          d_default(dflt),
-          d_string2enum(string2enum),
-          d_enum2string(enum2string)
-    {
-    }
+    /**
+     * Constructor.
+     *
+     * On construction, given uint64_t value determines the initial and the
+     * default enum value of the option.
+     *
+     * @param val         The initial and default value of the option.
+     * @param enum2string A map from option enum value to its string
+     *                    representation for the CLI.
+     */
+    Enum(uint64_t val, const Enum2StringMap& enum2string);
+    /** The current enum value of this option. */
     uint64_t d_value;
+    /** The default enum value of this option. */
     uint64_t d_default;
-    std::unordered_map<const char*, uint64_t> d_string2enum;
-    std::unordered_map<uint64_t, const char*> d_enum2string;
+    /** A map from enum value to its string representation for the CLI. */
+    Enum2StringMap d_enum2string;
+    /** A map from string representation for the CLI to enum value. */
+    String2EnumMap d_string2enum;
   };
 
+  /** The value data of options with numeric values. */
   struct Numeric
   {
-    Numeric(uint64_t val, uint64_t dflt, uint64_t min, uint64_t max)
-        : d_value(val), d_default(dflt), d_min(min), d_max(max)
+    /**
+     * Constructor.
+     *
+     * On construction, given uint64_t value determines the initial and the
+     * default integer value of a numeric option.
+     *
+     * @param val The initial and default value of a numeric option, must be
+     *            `>= min` and `<= max`.
+     * @param min The minimum value of the option.
+     * @param max The maximum value of the option.
+     */
+    Numeric(uint64_t val, uint64_t min, uint64_t max)
+        : d_value(val), d_default(val), d_min(min), d_max(max)
     {
     }
+    /** The current value of a numeric options. */
     uint64_t d_value;
+    /** The default value of a numeric option. */
     uint64_t d_default;
+    /** The minimum value of a numeric option. */
     uint64_t d_min;
+    /** The maximum value of a numeric option. */
     uint64_t d_max;
   };
 
-  OptionInfo() {}
-
+  /**
+   * Constructor for Boolean options.
+   * @param value     The initial and default value of the option.
+   * @param desc      The option description (used for CLI help message).
+   * @param lng       The long name of the option (`--<lng>` for the CLI).
+   * @param shrt      The short name of the option (`-<shrt>` for the CLI).
+   * @param is_expert True if this is an expert option.
+   */
   OptionInfo(bool value,
              const char* desc,
              const char* lng,
-             const char* shrt = nullptr)
-      : d_value(Boolean(value, value)),
+             const char* shrt = nullptr,
+             bool is_expert   = false)
+      : d_value(Boolean(value)),
         d_description(desc),
         d_long(lng),
-        d_short(shrt)
+        d_short(shrt),
+        d_is_expert(is_expert)
   {
   }
+  /**
+   * Constructor for options that take enum values.
+   * @param value       The initial and default value of the option.
+   * @param enum2string A map from option enum value to its string
+   *                    representation for the CLI.
+   * @param desc        The option description (used for the CLI help message).
+   * @param lng         The long name of the option (`--<lng>` in the CLI).
+   * @param shrt        The short name of the option (`-<shrt>` in the CLI).
+   * @param is_expert   True if this is an expert option.
+   */
   OptionInfo(uint64_t value,
-             const std::unordered_map<const char*, uint64_t> string2enum,
-             const std::unordered_map<uint64_t, const char*> enum2string,
+             const Enum2StringMap& enum2string,
              const char* desc,
              const char* lng,
-             const char* shrt = nullptr)
-      : d_value(Enum(value, value, string2enum, enum2string)),
+             const char* shrt = nullptr,
+             bool is_expert   = false)
+      : d_value(Enum(value, enum2string)),
         d_description(desc),
         d_long(lng),
-        d_short(shrt)
+        d_short(shrt),
+        d_is_expert(is_expert)
   {
   }
+  /**
+   * Constructor for numeric options.
+   * @param value     The initial and default value of the option.
+   * @param min       The minimum value of the option.
+   * @param max       The maximum value of the option.
+   * @param desc      The option description (used for the CLI help message).
+   * @param lng       The long name of the option (`--<lng>` in the CLI).
+   * @param shrt      The short name of the option (`-<shrt>` in the CLI).
+   * @param is_expert True if this is an expert option.
+   */
   OptionInfo(uint64_t value,
              uint64_t min,
              uint64_t max,
              const char* desc,
              const char* lng,
-             const char* shrt = nullptr)
-      : d_value(Numeric(value, value, min, max)),
+             const char* shrt = nullptr,
+             bool is_expert   = false)
+      : d_value(Numeric(value, min, max)),
         d_description(desc),
         d_long(lng),
-        d_short(shrt)
+        d_short(shrt),
+        d_is_expert(is_expert)
   {
   }
+  /** Default constructor. */
+  OptionInfo() {}
 
+  /** The option value data. */
   std::variant<std::monostate, Boolean, Enum, Numeric> d_value;
+  /** The option description. */
   const char* d_description;
+  /** The long name of the option (`--<lng>` in the CLI). */
   const char* d_long;
+  /** The short name of the option (`-<shrt>` in the CLI). */
   const char* d_short;
+  /** True if this is an expert option. */
+  bool d_is_expert;
 };
 
-struct Options
+/* -------------------------------------------------------------------------- */
+
+class Options
 {
-  bool incremental;
-  uint64_t log_level;
-  SatSolver sat_solver;
+ public:
+  /** Constructor. */
+  Options();
 
-  Options() : d_info(static_cast<size_t>(Option::NUM_OPTIONS))
-  {
-    init(Option::INCREMENTAL,
-         incremental,
-         false,
-         "incremental usage",
-         "incremental",
-         "i");
-    init(Option::LOGLEVEL, log_level, 0, 0, 3, "log level", "loglevel", "l");
-    init(Option::SAT_ENGINE,
-         sat_solver,
-         SatSolver::CADICAL,
-         {{"cadical", SatSolver::CADICAL},
-          {"cms", SatSolver::CRYPTOMINISAT},
-          {"kissat", SatSolver::KISSAT},
-          {"lingeling", SatSolver::LINGELING}},
-         {{SatSolver::CADICAL, "cadical"},
-          {SatSolver::CRYPTOMINISAT, "cms"},
-          {SatSolver::KISSAT, "kissat"},
-          {SatSolver::LINGELING, "lingeling"}},
-         "sat solver",
-         "sat-engine",
-         "SE");
-  }
-
+  /**
+   * Initialize and add Boolean option.
+   *
+   * @param name      The option to add.
+   * @param option    The member that serves as a short cut to the current
+   *                  value of the option.
+   * @param value     The initial and default value of the option.
+   * @param desc      The option description (used for CLI help message).
+   * @param lng       The long name of the option (`--<lng>` for the CLI).
+   * @param shrt      The short name of the option (`-<shrt>` for the CLI).
+   * @param is_expert True if this is an expert option.
+   */
   void init(Option name,
             bool& option,
             bool value,
             const char* desc,
             const char* lng,
-            const char* shrt = nullptr)
-  {
-    d_num_initialized++;
-    option                            = value;
-    d_info[static_cast<size_t>(name)] = {value, desc, lng, shrt};
-  }
+            const char* shrt = nullptr,
+            bool is_expert   = false);
 
+  /**
+   * Initialize and add option that takes enum values.
+   *
+   * @param name      The option to add.
+   * @param option    The member that serves as a short cut to the current
+   *                  value of the option.
+   * @param value       The initial and default value of the option.
+   * @param enum2string A map from option enum value to its string
+   *                    representation for the CLI.
+   * @param desc        The option description (used for the CLI help message).
+   * @param lng         The long name of the option (`--<lng>` in the CLI).
+   * @param shrt        The short name of the option (`-<shrt>` in the CLI).
+   * @param is_expert   True if this is an expert option.
+   */
   template <typename T>
   void init(Option name,
             T& option,
             T value,
-            const std::unordered_map<const char*, uint64_t> string2enum,
-            const std::unordered_map<uint64_t, const char*> enum2string,
+            const OptionInfo::Enum2StringMap& enum2string,
             const char* desc,
             const char* lng,
-            const char* shrt = nullptr)
-  {
-    d_num_initialized++;
-    option                            = value;
-    d_info[static_cast<size_t>(name)] = {static_cast<uint64_t>(value),
-                                         string2enum,
-                                         enum2string,
-                                         desc,
-                                         lng,
-                                         shrt};
-  }
+            const char* shrt = nullptr,
+            bool is_expert   = false);
 
+  /**
+   * Initialize and add numeric option.
+   *
+   * @param name      The option to add.
+   * @param option    The member that serves as a short cut to the current
+   *                  value of the option.
+   * @param value     The initial and default value of the option.
+   * @param min       The minimum value of the option.
+   * @param max       The maximum value of the option.
+   * @param desc      The option description (used for the CLI help message).
+   * @param lng       The long name of the option (`--<lng>` in the CLI).
+   * @param shrt      The short name of the option (`-<shrt>` in the CLI).
+   * @param is_expert True if this is an expert option.
+   */
   void init(Option name,
             uint64_t& option,
             uint64_t value,
@@ -180,18 +264,38 @@ struct Options
             uint64_t max,
             const char* desc,
             const char* lng,
-            const char* shrt = nullptr)
+            const char* shrt = nullptr,
+            bool is_expert   = false);
+
+  /** @return current value data of given option. */
+  template <typename T>
+  const T& operator[](Option opt) const
   {
-    d_num_initialized++;
-    option                            = value;
-    d_info[static_cast<size_t>(name)] = {value, min, max, desc, lng, shrt};
+    return std::get<T>(d_options[static_cast<size_t>(opt)].d_value);
   }
 
+  /** @return The current value of Boolean option `opt`. */
+  bool get_option_bool(Option opt) const
+  {
+    return this->operator[]<OptionInfo::Boolean>(opt).d_value;
+  }
+
+  /* Short cuts to default values of options. ----------------------- */
+  /** Option::INCREMENTAL */
+  bool incremental;
+  /** Option::LOG_LEVEL */
+  uint64_t log_level;
+  /** Option::SAT_SOLVER */
+  SatSolver sat_solver;
+
  private:
+  /** The number of added and initialized options. */
   size_t d_num_initialized = 0;
-  std::vector<OptionInfo> d_info;
+  /** The options. */
+  std::vector<OptionInfo> d_options;
 };
 
+/* -------------------------------------------------------------------------- */
 }  // namespace bzla::options
 
 #endif
