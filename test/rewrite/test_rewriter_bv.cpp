4f74c83e42daa1f8cfc2a4227267c8cd53003605
Mathias Preiner <mathias.preiner@gmail.com>
2022-09-23 11:06:05 -0700

    test: Add BV elimination rule testing infrastructure.

--- test/rewrite/test_rewriter_bv.cpp
+++ test/rewrite/test_rewriter_bv.cpp
@@ -7,6 +7,8 @@
 
 namespace bzla::test {
 
+static const char* s_solver_binary = std::getenv("SOLVER_BINARY");
+
 using namespace bzla::node;
 
 class TestRewriter : public ::testing::Test
@@ -14,6 +16,86 @@ class TestRewriter : public ::testing::Test
   void SetUp() override { d_bv_type = d_nm.mk_bv_type(4); }
 
  protected:
+  static std::string check_sat(std::stringstream& ss)
+  {
+    std::stringstream bench;
+    bench << "(set-logic QF_BV)\n";
+    bench << "(set-option :produce-models true)\n";
+    bench << ss.str();
+    bench << "(check-sat)\n";
+    bench << "(get-model)\n";
+
+    char filename[] = "bzlarwtest-XXXXXX";
+    int fd          = mkstemp(filename);
+    assert(fd != -1);
+
+    FILE* file = fdopen(fd, "w");
+    fputs(bench.str().c_str(), file);
+    fflush(file);
+
+    std::stringstream cmd;
+    cmd << s_solver_binary << " " << filename;
+
+    // Execute solver and read output.
+    FILE* fp = popen(cmd.str().c_str(), "r");
+    char buf[1024];
+    std::stringstream output;
+    while (fgets(buf, 1024, fp))
+    {
+      output << buf;
+    }
+    remove(filename);
+    fclose(file);
+
+    std::string result = output.str();
+    size_t newline_pos = result.find_last_of('\n');
+    return result.substr(0, newline_pos);
+  }
+
+  void test_elim_rule(Kind kind)
+  {
+    if (s_solver_binary == nullptr)
+    {
+      GTEST_SKIP_("SOLVER_BINARY environment variable not set.");
+    }
+
+    size_t num_children = s_node_kind_info[kind].num_children;
+    size_t num_indices  = s_node_kind_info[kind].num_indices;
+
+    NodeManager& nm = NodeManager::get();
+    Type bv8        = nm.mk_bv_type(8);
+    std::vector<Node> children;
+    std::vector<uint64_t> indices;
+    if (num_children >= 1)
+    {
+      children.push_back(nm.mk_const(bv8, "a"));
+    }
+    if (num_children >= 2)
+    {
+      children.push_back(nm.mk_const(bv8, "b"));
+    }
+
+    if (num_indices == 1)
+    {
+      indices.push_back(7);
+    }
+    if (num_indices == 2)
+    {
+      indices.push_back(1);
+    }
+
+    Node node = nm.mk_node(kind, children, indices);
+
+    std::stringstream ss;
+    for (const Node& child : children)
+    {
+      ss << "(declare-const " << child << " " << child.type() << ")\n";
+    }
+    ss << "(assert (distinct " << node << " " << d_rewriter.rewrite(node)
+       << "))\n";
+    ASSERT_EQ(check_sat(ss), "unsat");
+  }
+
   Rewriter d_rewriter;
   NodeManager& d_nm = NodeManager::get();
   Type d_bv_type;
