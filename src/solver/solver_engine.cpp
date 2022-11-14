53c63ed11b25c066b8a9e573eb8d32830f360437
Mathias Preiner <mathias.preiner@gmail.com>
2022-11-14 13:28:55 -0800

    solver: Add solver engine.

---
+++ src/solver/solver_engine.cpp
@@ -0,0 +1,245 @@
+#include "solver/solver_engine.h"
+
+#include "solving_context.h"
+
+namespace bzla {
+
+using namespace node;
+
+/* --- SolverEngine public -------------------------------------------------- */
+
+SolverEngine::SolverEngine(SolvingContext& context)
+    : d_context(context),
+      d_pop_callback(context.backtrack_mgr(), &d_backtrack_mgr),
+      d_assertions(context.assertions()),
+      d_register_cache(&d_backtrack_mgr),
+      d_sat_state(Result::UNKNOWN),
+      d_bv_solver(*this)
+{
+}
+
+Result
+SolverEngine::solve()
+{
+  // Process unprocessed assertions.
+  process_assertions();
+
+  do
+  {
+    // Process lemmas generated in previous iteration.
+    process_lemmas();
+
+    d_sat_state = d_bv_solver.check();
+    if (d_sat_state == Result::UNSAT)
+    {
+      break;
+    }
+    assert(d_sat_state == Result::SAT);
+    // d_sat_state = d_fp_solver.check();
+    // if (d_sat_state == Result::UNSAT)
+    //{
+    //   break;
+    // }
+    // d_sat_state = d_array_solver.check();
+    // if (d_sat_state == Result::UNSAT)
+    //{
+    //   break;
+    // }
+    // d_sat_state = d_uf_solver.check();
+    // if (d_sat_state == Result::UNSAT)
+    //{
+    //   break;
+    // }
+    // d_sat_state = d_quant_solver.check();
+    // if (d_sat_state == Result::UNSAT)
+    //{
+    //   break;
+    // }
+  } while (!d_lemmas.empty());
+
+  return d_sat_state;
+}
+
+Node
+SolverEngine::value(const Node& term)
+{
+  assert(d_sat_state == Result::SAT);
+
+  const Type& type = term.type();
+  if (type.is_bool() || type.is_bv())
+  {
+    return d_bv_solver.value(term);
+  }
+  else if (type.is_fp() || type.is_rm())
+  {
+    // return d_fp_solver.value(term);
+  }
+  else if (type.is_array())
+  {
+    // return d_array_solver.value(term);
+  }
+  else if (type.is_fun())
+  {
+    // return d_fun_solver.value(term);
+  }
+  assert(false);
+  return Node();
+}
+
+void
+SolverEngine::lemma(const Node& lemma)
+{
+  auto [it, inserted] = d_lemma_cache.insert(lemma);
+  assert(inserted);
+  d_lemmas.push_back(lemma);
+}
+
+Rewriter&
+SolverEngine::rewriter()
+{
+  return d_context.rewriter();
+}
+
+const option::Options&
+SolverEngine::options() const
+{
+  return d_context.options();
+}
+
+backtrack::BacktrackManager*
+SolverEngine::backtrack_mgr()
+{
+  return &d_backtrack_mgr;
+}
+
+/* --- SolverEngine private ------------------------------------------------- */
+
+bool
+SolverEngine::is_fp_leaf(const Node& term)
+{
+  Kind k = term.kind();
+  return k == Kind::FP_IS_INF || k == Kind::FP_IS_NAN || k == Kind::FP_IS_NEG
+         || k == Kind::FP_IS_NORM || k == Kind::FP_IS_POS
+         || k == Kind::FP_IS_SUBNORM || k == Kind::FP_IS_ZERO
+         || k == Kind::FP_EQUAL || k == Kind::FP_LE || k == Kind::FP_LT
+         || k == Kind::FP_TO_SBV || k == Kind::FP_TO_UBV
+         || (k == Kind::EQUAL
+             && (term[0].type().is_fp() || term[0].type().is_rm()));
+}
+
+bool
+SolverEngine::is_array_leaf(const Node& term)
+{
+  Kind k = term.kind();
+  return k == Kind::SELECT || (k == Kind::EQUAL && (term[0].type().is_array()));
+}
+
+bool
+SolverEngine::is_fun_leaf(const Node& term)
+{
+  Kind k = term.kind();
+  return k == Kind::APPLY || (k == Kind::EQUAL && (term[0].type().is_fun()));
+}
+
+bool
+SolverEngine::is_quant_leaf(const Node& term)
+{
+  return term.kind() == Kind::FORALL;
+}
+
+void
+SolverEngine::sync_scope(size_t level)
+{
+  while (d_backtrack_mgr.num_levels() < level)
+  {
+    d_backtrack_mgr.push();
+  }
+}
+
+void
+SolverEngine::process_assertions()
+{
+  while (!d_assertions.empty())
+  {
+    size_t level = d_assertions.level(d_assertions.begin());
+
+    // Sync backtrack manager to level. This is required if there are levels
+    // that do not contain any assertions.
+    sync_scope(level);
+
+    // Create vector for current level
+    preprocess::AssertionVector assertions(d_assertions);
+    for (size_t i = 0, size = assertions.size(); i < size; ++i)
+    {
+      process_assertion(assertions[i], level);
+    }
+
+    // Advance assertions to next level
+    d_assertions.set_index(d_assertions.begin() + assertions.size());
+  }
+  assert(d_assertions.empty());
+
+  // Sync backtrack manager to level. This is required if there are levels
+  // that do not contain any assertions.
+  sync_scope(d_context.backtrack_mgr()->num_levels());
+}
+
+void
+SolverEngine::process_assertion(const Node& assertion, size_t level)
+{
+  // Send assertion to bit-vector solver.
+  if (d_register_cache.find(assertion) == d_register_cache.end())
+  {
+    d_bv_solver.register_assertion(assertion, level);
+  }
+
+  // Send theory leafs to corresponding solvers.
+  node::node_ref_vector visit{assertion};
+  do
+  {
+    const Node& cur = visit.back();
+    visit.pop_back();
+
+    auto [it, inserted] = d_register_cache.insert(cur);
+    if (inserted)
+    {
+      if (is_fp_leaf(cur))
+      {
+        assert(false);
+        // d_fp_solver.register_term(cur);
+      }
+      else if (is_array_leaf(cur))
+      {
+        assert(false);
+        // d_array_solver.register_term(cur);
+      }
+      else if (is_fun_leaf(cur))
+      {
+        assert(false);
+        // d_fun_solver.register_term(cur);
+      }
+      else if (is_quant_leaf(cur))
+      {
+        assert(false);
+        // d_quant_solver.register_term(cur);
+      }
+      else
+      {
+        visit.insert(visit.end(), cur.begin(), cur.end());
+      }
+    }
+  } while (!visit.empty());
+}
+
+void
+SolverEngine::process_lemmas()
+{
+  for (const Node& lemma : d_lemmas)
+  {
+    // TODO: check if this is what we want
+    process_assertion(lemma, 0);
+  }
+  d_lemmas.clear();
+}
+
+}  // namespace bzla
---
+++ src/solver/solver_engine.h
@@ -0,0 +1,102 @@
+#ifndef BZLA_SOLVER_SOLVER_ENGINE_H_INCLUDED
+#define BZLA_SOLVER_SOLVER_ENGINE_H_INCLUDED
+
+#include "backtrack/assertion_stack.h"
+#include "backtrack/backtrackable.h"
+#include "backtrack/pop_callback.h"
+#include "backtrack/unordered_set.h"
+#include "node/node.h"
+#include "rewrite/rewriter.h"
+#include "solver/bv/bv_solver.h"
+#include "solver/result.h"
+
+namespace bzla {
+
+class SolvingContext;
+
+class SolverEngine
+{
+  friend SolvingContext;
+
+ public:
+  SolverEngine(SolvingContext& context);
+
+  /** Get value of given term. Queries corresponding solver for value. */
+  Node value(const Node& term);
+
+  /** Add a lemma. */
+  void lemma(const Node& lemma);
+
+  /** @return Rewriter. */
+  Rewriter& rewriter();
+
+  /** @return Options. */
+  const option::Options& options() const;
+
+  /** @return Solver engine backtrack manager. */
+  backtrack::BacktrackManager* backtrack_mgr();
+
+ private:
+  // temporary helpers, should be moved to corresponding solvers as static
+  // method
+  static bool is_fp_leaf(const Node& term);
+  static bool is_array_leaf(const Node& term);
+  static bool is_fun_leaf(const Node& term);
+  static bool is_quant_leaf(const Node& term);
+
+  /**
+   * Solve current set of assertions.
+   *
+   * @note Should only be called by SolvingContext, hence the friend
+   *       declaration.
+   */
+  Result solve();
+
+  /** Synchronize d_backtrack_mgr up to given level. */
+  void sync_scope(size_t level);
+
+  /**
+   * Process current set of assertions via process_assertion().
+   */
+  void process_assertions();
+
+  /**
+   * Processes given assertion and distributes reachable theory leafs to
+   * solvers.
+   */
+  void process_assertion(const Node& assertion, size_t level);
+
+  /** Process lemmas added via lemma(). */
+  void process_lemmas();
+
+  /** Associated solving context. */
+  SolvingContext& d_context;
+
+  /** Solver engine backtrack manager. */
+  backtrack::BacktrackManager d_backtrack_mgr;
+  /** Callback to sync with solving context backtrack manager on pop(). */
+  backtrack::PopCallback d_pop_callback;
+  /** Assertion view of unprocessed assertions. */
+  backtrack::AssertionView& d_assertions;
+  /** Cache used by process_assertion(). */
+  backtrack::unordered_set<Node> d_register_cache;
+
+  /** Lemmas added via lemma(). */
+  std::vector<Node> d_lemmas;
+  /** Lemma cache. */
+  std::unordered_set<Node> d_lemma_cache;
+
+  /** Result of latest solve() call. */
+  Result d_sat_state;
+
+  /** Theory solvers. */
+  bv::BvSolver d_bv_solver;
+  // fp::FpSolver d_fp_solver;
+  // fun::FunSolver d_fun_solver;
+  // array::ArraySolver d_array_solver;
+  // quant::QuantSolver d_quant_solver;
+};
+
+}  // namespace bzla
+
+#endif
