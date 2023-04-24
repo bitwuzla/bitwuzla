ca62ab43e69531dee1cc819f15e87e9a06148b62
Aina Niemetz <aina.niemetz@gmail.com>
2023-04-24 16:45:40 -0700

    parser: btor2: Add token enum class.

---
+++ src/parser/btor2/token.cpp
@@ -0,0 +1,97 @@
+#include "parser/btor2/token.h"
+
+namespace bzla {
+namespace parser::btor2 {
+
+std::ostream&
+operator<<(std::ostream& out, Token token)
+{
+  out << std::to_string(token);
+  return out;
+}
+}
+}
+
+namespace std {
+
+std::string
+to_string(bzla::parser::btor2::Token token)
+{
+  using namespace bzla::parser::btor2;
+  switch (token)
+  {
+    case Token::INVALID: return "<invalid token>";
+    case Token::ADD: return "add";
+    case Token::AND: return "and";
+    case Token::ARRAY: return "array";
+    case Token::BAD: return "bad";
+    case Token::BITVEC: return "bitvec";
+    case Token::CONCAT: return "concat";
+    case Token::CONST: return "const";
+    case Token::CONSTRAINT: return "constraint";
+    case Token::CONSTD: return "constd";
+    case Token::CONSTH: return "consth";
+    case Token::DEC: return "dec";
+    case Token::EQ: return "eq";
+    case Token::FAIR: return "fair";
+    case Token::IFF: return "iff";
+    case Token::IMPLIES: return "implies";
+    case Token::INC: return "inc";
+    case Token::INIT: return "init";
+    case Token::INPUT: return "input";
+    case Token::ITE: return "ite";
+    case Token::JUSTICE: return "justice";
+    case Token::MUL: return "mul";
+    case Token::NAND: return "nand";
+    case Token::NEQ: return "neq";
+    case Token::NEG: return "neg";
+    case Token::NEXT: return "next";
+    case Token::NOR: return "nor";
+    case Token::NOT: return "not";
+    case Token::ONE: return "one";
+    case Token::ONES: return "ones";
+    case Token::OR: return "or";
+    case Token::OUTPUT: return "output";
+    case Token::READ: return "read";
+    case Token::REDAND: return "redand";
+    case Token::REDOR: return "redor";
+    case Token::REDXOR: return "redxor";
+    case Token::ROL: return "rol";
+    case Token::ROR: return "ror";
+    case Token::SADDO: return "saddo";
+    case Token::SDIV: return "sdiv";
+    case Token::SDIVO: return "sdivo";
+    case Token::SEXT: return "sext";
+    case Token::SGT: return "sgt";
+    case Token::SGTE: return "sgte";
+    case Token::SLICE: return "slice";
+    case Token::SLL: return "sll";
+    case Token::SLT: return "slt";
+    case Token::SLTE: return "slte";
+    case Token::SORT: return "sort";
+    case Token::SMOD: return "smod";
+    case Token::SMULO: return "smulo";
+    case Token::SRA: return "sra";
+    case Token::SREM: return "rem";
+    case Token::SRL: return "srl";
+    case Token::SSUBO: return "ssubo";
+    case Token::STATE: return "state";
+    case Token::SUB: return "sub";
+    case Token::UADDO: return "uaddo";
+    case Token::UDIV: return "udiv";
+    case Token::UEXT: return "uext";
+    case Token::UGT: return "ugt";
+    case Token::UGTE: return "ugte";
+    case Token::ULT: return "ult";
+    case Token::ULTE: return "ulte";
+    case Token::UMULO: return "umulo";
+    case Token::UREM: return "urem";
+    case Token::USUBO: return "usubo";
+    case Token::WRITE: return "write";
+    case Token::XNOR: return "xnor";
+    case Token::XOR: return "xor";
+    case Token::ZERO: return "zero";
+  }
+  return "<unsupported token>";
+}
+}
---
+++ src/parser/btor2/token.h
@@ -0,0 +1,91 @@
+#ifndef BZLA_PARSER_BTOR2_TOKEN_H_INCLUDED
+#define BZLA_PARSER_BTOR2_TOKEN_H_INCLUDED
+
+#include <cstdint>
+#include <iostream>
+
+namespace bzla {
+namespace parser::btor2 {
+
+enum class Token {
+  INVALID,
+  ADD,
+  AND,
+  ARRAY,
+  BAD,
+  BITVEC,
+  CONCAT,
+  CONST,
+  CONSTRAINT,
+  CONSTD,
+  CONSTH,
+  DEC,
+  EQ,
+  FAIR,
+  IFF,
+  IMPLIES,
+  INC,
+  INIT,
+  INPUT,
+  ITE,
+  JUSTICE,
+  MUL,
+  NAND,
+  NEQ,
+  NEG,
+  NEXT,
+  NOR,
+  NOT,
+  ONE,
+  ONES,
+  OR,
+  OUTPUT,
+  READ,
+  REDAND,
+  REDOR,
+  REDXOR,
+  ROL,
+  ROR,
+  SADDO,
+  SDIV,
+  SDIVO,
+  SEXT,
+  SGT,
+  SGTE,
+  SLICE,
+  SLL,
+  SLT,
+  SLTE,
+  SORT,
+  SMOD,
+  SMULO,
+  SRA,
+  SREM,
+  SRL,
+  SSUBO,
+  STATE,
+  SUB,
+  UADDO,
+  UDIV,
+  UEXT,
+  UGT,
+  UGTE,
+  ULT,
+  ULTE,
+  UMULO,
+  UREM,
+  USUBO,
+  WRITE,
+  XNOR,
+  XOR,
+  ZERO,
+};
+
+std::ostream& operator<<(std::ostream& out, Token token);
+}
+}
+
+namespace std {
+std::string to_string(bzla::parser::btor2::Token token);
+}
+#endif
