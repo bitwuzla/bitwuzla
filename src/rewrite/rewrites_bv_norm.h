dd056ad13001fc18a1bf926ca652a5adb2ceb287
Aina Niemetz <aina.niemetz@gmail.com>
2023-05-24 13:35:27 -0700

    rewriter: bv: Delete obsolete normalization rewrite.

--- src/rewrite/rewrites_bv_norm.h
+++ src/rewrite/rewrites_bv_norm.h
@@ -5,13 +5,6 @@
 
 namespace bzla {
 
-/* bvadd -------------------------------------------------------------------- */
-
-// const_neg_lhs_add
-// const_neg_rhs_add
-template <>
-Node RewriteRule<RewriteRuleKind::BV_ADD_NORM_MUL_CONST>::_apply(
-    Rewriter& rewriter, const Node& node);
 
 }  // namespace bzla
 
