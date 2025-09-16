458afa34450d23cf862d3233e0b98d19e7601d34
Mathias Preiner <mathias.preiner@gmail.com>
2025-09-16 11:09:57 -0700

    sat: kissat: Allow incremental usage.
    This will create a new Kissat instance for each solve call.

--- src/sat/kissat.cpp
+++ src/sat/kissat.cpp
@@ -35,25 +35,20 @@ kissat_terminate_wrapper(void* state)
 
 /*------------------------------------------------------------------------*/
 
-Kissat::Kissat()
-{
-  d_solver = kissat_init();
-  kissat_set_option(d_solver, "quiet", 1);
-}
+Kissat::Kissat() { init(); }
 
 Kissat::~Kissat() { kissat_release(d_solver); }
 
 void
 Kissat::add(int32_t lit)
 {
-  kissat_add(d_solver, lit);
+  d_literals.push_back(lit);
 }
 
 void
 Kissat::assume(int32_t lit)
 {
-  (void) lit;
-  throw Error("Incremental solving not supported in Kissat");
+  d_assumptions.push_back(lit);
 }
 
 int32_t
@@ -84,7 +79,27 @@ Kissat::fixed(int32_t lit)
 Result
 Kissat::solve()
 {
+  if (d_init)
+  {
+    init();
+  }
+  size_t size_before_assumptions = d_literals.size();
+
+  for (const auto a : d_assumptions)
+  {
+    d_literals.push_back(a);
+    d_literals.push_back(0);
+  }
+  d_assumptions.clear();
+
+  for (const auto& lit : d_literals)
+  {
+    kissat_add(d_solver, lit);
+  }
+
+  d_literals.resize(size_before_assumptions);
   int32_t res = kissat_solve(d_solver);
+  d_init      = true;
   if (res == 10) return Result::SAT;
   if (res == 20) return Result::UNSAT;
   return Result::UNKNOWN;
@@ -104,6 +119,18 @@ const char *
 Kissat::get_version() const
 {
   return kissat_version();
+}
+
+void
+Kissat::init()
+{
+  if (d_solver)
+  {
+    kissat_release(d_solver);
+  }
+  d_solver = kissat_init();
+  kissat_set_option(d_solver, "quiet", 1);
+  d_init = false;
 }
 
 /*------------------------------------------------------------------------*/
