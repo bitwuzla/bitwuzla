8c3e56736495f17d291fb511bbe66771a04acc43
Aina Niemetz <aina.niemetz@gmail.com>
2025-11-20 18:29:15 -0800

    bv inverter: Add IC for BV_MUL.

--- src/solver/bv/bv_inverter.cpp
+++ src/solver/bv/bv_inverter.cpp
@@ -12,6 +12,8 @@
 
 #include <cassert>
 
+#include "node/node_kind.h"
+
 namespace bzla::bv {
 
 /* --- BvInverter public ---------------------------------------------------- */
@@ -21,19 +23,23 @@ BvInverter::BvInverter(Env& env) : d_nm(env.nm()) {}
 BvInverter::~BvInverter() {}
 
 Node
-BvInverter::ic(Kind kind, const Node& x, const Node& s, const Node& t)
+BvInverter::ic(Kind predicate, Kind kind, const std::vector<Node>& nodes)
 {
-  switch (kind)
+  assert(nodes.size() > 1);
+  switch (predicate)
   {
-    case Kind::BV_SGE:
-    case Kind::BV_SLE:
-    case Kind::BV_UGE:
-    case Kind::BV_ULE: return d_nm.mk_value(true);
+    case Kind::BV_SLT: return ic_bv_slt(kind, nodes);
+    case Kind::BV_SLE: return ic_bv_sle(kind, nodes);
+    case Kind::BV_SGT: return ic_bv_sgt(kind, nodes);
+    case Kind::BV_SGE: return ic_bv_sge(kind, nodes);
 
-    case Kind::BV_SLT: return ic_bv_slt(x, t);
-    case Kind::BV_SGT: return ic_bv_sgt(x, t);
-    case Kind::BV_ULT: return ic_bv_ult(x, t);
-    case Kind::BV_UGT: return ic_bv_ugt(x, t);
+    case Kind::BV_ULT: return ic_bv_ult(kind, nodes);
+    case Kind::BV_ULE: return ic_bv_ule(kind, nodes);
+    case Kind::BV_UGT: return ic_bv_ugt(kind, nodes);
+    case Kind::BV_UGE: return ic_bv_uge(kind, nodes);
+
+    case Kind::EQUAL: return ic_equal(kind, nodes);
+    case Kind::DISTINCT: return ic_distinct(kind, nodes);
 
     default: assert(false);
   }
@@ -42,39 +48,247 @@ BvInverter::ic(Kind kind, const Node& x, const Node& s, const Node& t)
 /* --- BvInverter private --------------------------------------------------- */
 
 Node
-BvInverter::ic_bv_slt(const Node& x, const Node& t)
+BvInverter::ic_equal(Kind kind, const std::vector<Node>& nodes)
 {
-  // IC: (distinct t min_signed_[w])
-  return d_nm.mk_node(
-      Kind::DISTINCT,
-      {t, d_nm.mk_value(BitVector::mk_min_signed(x.type().bv_size()))});
+  const Node& t = nodes.back();
+  const Node& s = nodes[nodes.size() - 2];
+  switch (kind)
+  {
+    case Kind::BV_MUL: assert(nodes.size() == 3); return ic_eq_mul(s, t);
+    default: assert(false);
+  }
 }
 
 Node
-BvInverter::ic_bv_sgt(const Node& x, const Node& t)
+BvInverter::ic_distinct(Kind kind, const std::vector<Node>& nodes)
 {
-  // IC: (distinct t max_signed_[w])
-  return d_nm.mk_node(
-      Kind::DISTINCT,
-      {t, d_nm.mk_value(BitVector::mk_max_signed(x.type().bv_size()))});
+  const Node& t = nodes.back();
+  const Node& s = nodes[nodes.size() - 2];
+  switch (kind)
+  {
+    case Kind::BV_MUL: assert(nodes.size() == 3); return ic_dist_mul(s, t);
+    default: assert(false);
+  }
 }
 
 Node
-BvInverter::ic_bv_ult(const Node& x, const Node& t)
+BvInverter::ic_bv_slt(Kind kind, const std::vector<Node>& nodes)
 {
-  // IC: (distinct t (_ bv0 w))
-  return d_nm.mk_node(
-      Kind::DISTINCT,
-      {t, d_nm.mk_value(BitVector::mk_zero(x.type().bv_size()))});
+  const Node& t = nodes.back();
+  const Node& s = nodes[nodes.size() - 2];
+  switch (kind)
+  {
+    case Kind::BV_MUL: assert(nodes.size() == 3); return ic_bv_slt_mul(s, t);
+    default:
+      // IC: (distinct t min_signed_[w])
+      return d_nm.mk_node(
+          Kind::DISTINCT,
+          {t, d_nm.mk_value(BitVector::mk_min_signed(s.type().bv_size()))});
+  }
 }
 
 Node
-BvInverter::ic_bv_ugt(const Node& x, const Node& t)
+BvInverter::ic_bv_sle(Kind kind, const std::vector<Node>& nodes)
 {
-  // IC: (distinct t (bvnot (_ bv0 w)))
+  const Node& t = nodes.back();
+  const Node& s = nodes[nodes.size() - 2];
+  switch (kind)
+  {
+    case Kind::BV_MUL: assert(nodes.size() == 3); return ic_bv_sle_mul(s, t);
+    default:
+      // IC: true
+      return d_nm.mk_value(true);
+  }
+}
+
+Node
+BvInverter::ic_bv_sgt(Kind kind, const std::vector<Node>& nodes)
+{
+  const Node& t = nodes.back();
+  const Node& s = nodes[nodes.size() - 2];
+  switch (kind)
+  {
+    case Kind::BV_MUL: assert(nodes.size() == 3); return ic_bv_sgt_mul(s, t);
+    default:
+      // IC: (distinct t max_signed_[w])
+      return d_nm.mk_node(
+          Kind::DISTINCT,
+          {t, d_nm.mk_value(BitVector::mk_max_signed(s.type().bv_size()))});
+  }
+}
+
+Node
+BvInverter::ic_bv_sge(Kind kind, const std::vector<Node>& nodes)
+{
+  const Node& t = nodes.back();
+  const Node& s = nodes[nodes.size() - 2];
+  switch (kind)
+  {
+    case Kind::BV_MUL: assert(nodes.size() == 3); return ic_bv_sge_mul(s, t);
+    default:
+      // IC: true
+      return d_nm.mk_value(true);
+  }
+}
+
+Node
+BvInverter::ic_bv_ult(Kind kind, const std::vector<Node>& nodes)
+{
+  const Node& t = nodes.back();
+  const Node& s = nodes[nodes.size() - 2];
+  switch (kind)
+  {
+    case Kind::BV_MUL: assert(nodes.size() == 3); return ic_bv_ult_mul(s, t);
+    default:
+      // IC: (distinct t (_ bv0 w))
+      return d_nm.mk_node(
+          Kind::DISTINCT,
+          {t, d_nm.mk_value(BitVector::mk_zero(s.type().bv_size()))});
+  }
+}
+
+Node
+BvInverter::ic_bv_ugt(Kind kind, const std::vector<Node>& nodes)
+{
+  const Node& t = nodes.back();
+  const Node& s = nodes[nodes.size() - 2];
+  switch (kind)
+  {
+    case Kind::BV_MUL: assert(nodes.size() == 3); return ic_bv_ugt_mul(s, t);
+    default:
+      // IC: (distinct t (bvnot (_ bv0 w)))
+      return d_nm.mk_node(
+          Kind::DISTINCT,
+          {t, d_nm.mk_value(BitVector::mk_ones(s.type().bv_size()))});
+  }
+}
+
+/* --- BV_MUL --------------------------------------------------------------- */
+
+Node
+BvInverter::ic_eq_mul(const Node& s, const Node& t)
+{
+  // x * s = t
+  // IC: (= (bvand (bvor (bvneg s) s) t) t)
+  return d_nm.mk_node(
+      Kind::EQUAL,
+      {d_nm.mk_node(
+           Kind::BV_AND,
+           {d_nm.mk_node(Kind::BV_OR, {d_nm.mk_node(Kind::BV_NEG, {s}), s})}),
+       t});
+}
+
+Node
+BvInverter::ic_dist_mul(const Node& s, const Node& t)
+{
+  // x * s = t
+  // IC: (or (distinct s 0_[w]) (distinct t 0_[w]))
+  Node zero = d_nm.mk_value(BitVector::mk_zero(s.type().bv_size()));
+  return d_nm.mk_node(Kind::OR,
+                      {d_nm.mk_node(Kind::DISTINCT, {s, zero}),
+                       d_nm.mk_node(Kind::DISTINCT, {t, zero})});
+}
+
+Node
+BvInverter::ic_bv_ult_mul(const Node& s, const Node& t)
+{
+  // x * s <_u t
+  // IC: (distinct t 0_[w])
   return d_nm.mk_node(
       Kind::DISTINCT,
-      {t, d_nm.mk_value(BitVector::mk_ones(x.type().bv_size()))});
+      {t, d_nm.mk_value(BitVector::mk_zero(s.type().bv_size()))});
 }
+
+Node
+BvInverter::ic_bv_ule_mul(const Node& s, const Node& t)
+{
+  (void) s;
+  (void) t;
+  // x * s <=_u t
+  // IC: true
+  return d_nm.mk_value(true);
+}
+
+Node
+BvInverter::ic_bv_ugt_mul(const Node& s, const Node& t)
+{
+  // x * s >_u t
+  // IC: (bvult t (bvor (bvneg s) s))
+  return d_nm.mk_node(
+      Kind::BV_ULT,
+      {t, d_nm.mk_node(Kind::BV_OR, {d_nm.mk_node(Kind::BV_NEG, {s}), s})});
+}
+
+Node
+BvInverter::ic_bv_uge_mul(const Node& s, const Node& t)
+{
+  // x * s >=_u t
+  // IC: (bvuge (bvor (bvneg s) s) t)
+  return d_nm.mk_node(
+      Kind::BV_UGE,
+      {d_nm.mk_node(Kind::BV_OR, {d_nm.mk_node(Kind::BV_NEG, {s}), s}), t});
+}
+
+Node
+BvInverter::ic_bv_slt_mul(const Node& s, const Node& t)
+{
+  // x * s <_s t
+  // IC: (bvslt (bvand (bvnot (bvneg t)) (bvor (bvneg s) s)) t)
+  return d_nm.mk_node(
+      Kind::BV_SLT,
+      {d_nm.mk_node(
+           Kind::BV_AND,
+           {d_nm.mk_node(Kind::BV_NOT, {d_nm.mk_node(Kind::BV_NEG, {t})}),
+            d_nm.mk_node(Kind::BV_OR, {d_nm.mk_node(Kind::BV_NEG, {s}), s})}),
+       t});
+}
+
+Node
+BvInverter::ic_bv_sle_mul(const Node& s, const Node& t)
+{
+  (void) s;
+  (void) t;
+  // x * s <=_s t
+  // IC: (not (and (= s z) (bvslt t s)))
+  return d_nm.mk_node(
+      Kind::NOT,
+      {d_nm.mk_node(
+          Kind::AND,
+          {d_nm.mk_node(
+               Kind::EQUAL,
+               {s, d_nm.mk_value(BitVector::mk_zero(s.type().bv_size()))}),
+           d_nm.mk_node(Kind::BV_SLT, {t, s})})});
+}
+
+Node
+BvInverter::ic_bv_sgt_mul(const Node& s, const Node& t)
+{
+  // x * s >_s t
+  // IC: (bvslt t (bvsub t (bvor (bvor s t) (bvneg s))))  */
+  return d_nm.mk_node(
+      Kind::BV_SLT,
+      {t,
+       d_nm.mk_node(Kind::BV_SUB,
+                    {t,
+                     d_nm.mk_node(Kind::BV_OR,
+                                  {d_nm.mk_node(Kind::BV_OR, {s, t}),
+                                   d_nm.mk_node(Kind::BV_NEG, {s})})})});
+}
+
+Node
+BvInverter::ic_bv_sge_mul(const Node& s, const Node& t)
+{
+  // x * s >=_s t
+  // IC: (bvsge (bvand (bvor (bvneg s) s) max) t)
+  return d_nm.mk_node(
+      Kind::BV_SGE,
+      {d_nm.mk_node(
+           Kind::BV_AND,
+           {d_nm.mk_node(Kind::BV_OR, {d_nm.mk_node(Kind::BV_NEG, {s}), s}),
+            d_nm.mk_value(BitVector::mk_max_signed(s.type().bv_size()))}),
+       t});
+}
+
+/* -------------------------------------------------------------------------- */
 
 }  // namespace bzla::bv
--- src/solver/bv/bv_inverter.h
+++ src/solver/bv/bv_inverter.h
@@ -27,13 +27,32 @@ class BvInverter
   BvInverter(Env& env);
   ~BvInverter();
 
-  Node ic(Kind kind, const Node& x, const Node& s, const Node& t);
+  Node ic(Kind predicate, Kind kind, const std::vector<Node>& nodes);
 
  private:
-  Node ic_bv_slt(const Node& x, const Node& t);
-  Node ic_bv_sgt(const Node& x, const Node& t);
-  Node ic_bv_ult(const Node& x, const Node& t);
-  Node ic_bv_ugt(const Node& x, const Node& t);
+  Node ic_equal(Kind kind, const std::vector<Node>& nodes);
+  Node ic_distinct(Kind kind, const std::vector<Node>& nodes);
+
+  Node ic_bv_slt(Kind kind, const std::vector<Node>& nodes);
+  Node ic_bv_sle(Kind kind, const std::vector<Node>& nodes);
+  Node ic_bv_sgt(Kind kind, const std::vector<Node>& nodes);
+  Node ic_bv_sge(Kind kind, const std::vector<Node>& nodes);
+
+  Node ic_bv_ult(Kind kind, const std::vector<Node>& nodes);
+  Node ic_bv_ule(Kind kind, const std::vector<Node>& nodes);
+  Node ic_bv_ugt(Kind kind, const std::vector<Node>& nodes);
+  Node ic_bv_uge(Kind kind, const std::vector<Node>& nodes);
+
+  Node ic_eq_mul(const Node& s, const Node& t);
+  Node ic_dist_mul(const Node& s, const Node& t);
+  Node ic_bv_slt_mul(const Node& s, const Node& t);
+  Node ic_bv_sle_mul(const Node& s, const Node& t);
+  Node ic_bv_sgt_mul(const Node& s, const Node& t);
+  Node ic_bv_sge_mul(const Node& s, const Node& t);
+  Node ic_bv_ult_mul(const Node& s, const Node& t);
+  Node ic_bv_ule_mul(const Node& s, const Node& t);
+  Node ic_bv_ugt_mul(const Node& s, const Node& t);
+  Node ic_bv_uge_mul(const Node& s, const Node& t);
 
   /** The associated node manager. */
   NodeManager& d_nm;
