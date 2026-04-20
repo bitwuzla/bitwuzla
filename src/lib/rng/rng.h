75fa4ecacf382b6954707798131b5fca79a12cbc
Aina Niemetz <aina.niemetz@gmail.com>
2026-04-20 05:33:08 -0700

    Refine docs for RNG::pick<T>() for float types.

--- src/lib/rng/rng.h
+++ src/lib/rng/rng.h
@@ -73,7 +73,7 @@ class RNG
   }
 
   /** Pick a floating point number with type T between 'from' and 'to'
-   * (exclusive). */
+   * ([from, to), upper bound exclusive). */
   template <
       typename T,
       typename std::enable_if<std::is_floating_point<T>::value, int>::type = 0>
